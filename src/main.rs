c2d01a1201aed574370bb6f8aaf8f63a68a04037
Ben Berman <ben.m.berman@gmail.com>
2016-11-03 00:27:28 -0400

    Actually writing good things to file!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::Write;
-
+use std::fs::OpenOptions;
 
 fn get_good_thing() -> String {
     print!("Say something nice to me: ");
@@ -14,9 +14,35 @@ fn get_good_thing() -> String {
     }
 }
 
+fn data_dir_path() -> String {
+    std::env::home_dir()
+        .map(|home_dir| home_dir.display().to_string() + "/.config/the-good-things")
+        .unwrap_or("/usr/share/the-good-things".to_string())
+}
+
+fn write_good_thing(string: String) {
+    let dir_path = data_dir_path();
+    let res = std::fs::create_dir_all(&dir_path)
+        .and_then(|_| {
+            println!("{}/good-things.txt", dir_path);
+            OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(dir_path + "/good-things.txt")
+        })
+    .and_then(|mut file| {
+        file.write_all(string.as_bytes())
+    });
+
+    match res {
+        Ok(_) => println!("Success?"),
+        Err(error) => println!("Couldn't write to file {}", error)
+    }
+}
+
 fn main() {
     for _ in 0..5 {
         let good_thing = get_good_thing();
-        println!("Got good thing {}", good_thing);
+        write_good_thing(good_thing);
     }
 }
