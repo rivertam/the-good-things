054241770398d201d8170627128ad7f391867baa
Ben Berman <ben.m.berman@gmail.com>
2016-11-06 10:48:06 -0500

    Wrote `fetch_good_things` correctly

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::io::Write;
 use std::fs::OpenOptions;
+use std::io::BufRead;
 
 fn get_good_thing() -> String {
     print!("Say something nice to me: ");
-    std::io::stdout().flush().ok().expect("whazza");
+    std::io::stdout().flush().expect("Couldn't flush stdout");
     let mut input = String::new();
     match std::io::stdin().read_line(&mut input) {
         Ok(_) => input,
@@ -14,33 +15,68 @@ fn get_good_thing() -> String {
     }
 }
 
+static DATA_FILE_NAME: &'static str = "good-things.txt";
+
 fn data_dir_path() -> String {
     std::env::home_dir()
         .map(|home_dir| home_dir.display().to_string() + "/.config/the-good-things")
         .unwrap_or("/usr/share/the-good-things".to_string())
 }
 
+fn data_file_path() -> String {
+    data_dir_path() + "/" + DATA_FILE_NAME
+}
+
+fn fetch_good_things() -> Vec<String> {
+    let res = std::fs::File::open(data_file_path())
+        .and_then(|f| {
+            let file = std::io::BufReader::new(&f);
+            let mut vec = std::vec::Vec::new();
+            for line in file.lines() {
+                match line {
+                    Ok(l) => vec.push(l),
+                    Err(_) => continue,
+                }
+            }
+
+            return Ok(vec)
+        });
+
+    match res {
+        Ok(v) => v,
+        Err(e) => {
+            println!("Couldn't open good things file");
+            println!("Located at {}", data_file_path());
+            println!("Error: {}", e);
+            std::vec::Vec::new()
+        },
+    }
+}
+
 fn write_good_thing(string: String) {
     let dir_path = data_dir_path();
     let res = std::fs::create_dir_all(&dir_path)
         .and_then(|_| {
-            println!("{}/good-things.txt", dir_path);
             OpenOptions::new()
                 .create(true)
                 .append(true)
-                .open(dir_path + "/good-things.txt")
-        })
-    .and_then(|mut file| {
-        file.write_all(string.as_bytes())
-    });
+                .open(data_file_path())
+        }).and_then(|mut file| {
+            file.write_all(string.as_bytes())
+        });
 
     match res {
-        Ok(_) => println!("Success?"),
+        Ok(_) => return,
         Err(error) => println!("Couldn't write to file {}", error)
     }
 }
 
 fn main() {
+    for good_thing in fetch_good_things() {
+        println!("{}", good_thing);
+    }
+
+
     for _ in 0..5 {
         let good_thing = get_good_thing();
         write_good_thing(good_thing);
